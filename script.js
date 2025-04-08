e6c912687a69c70daa4b41c5c035f9290d13f858
WilliamAlvesOliveira <27will.ramone@gmail.com>
2025-04-08 12:15:32 -0300

    implementação de temas

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+function atualizarMenuPosition() {
+    const header = document.querySelector('header');
+    const altura = header.offsetHeight + 'px';
+    document.documentElement.style.setProperty('--menuPosition', altura);
+  }
+  
+  window.addEventListener('load', atualizarMenuPosition);
+
+  window.addEventListener('resize', atualizarMenuPosition);
+
+function ativarTemaClaro() {
+    const root = document.documentElement;
+    root.style.setProperty('--bg-color', '#f8f9fa');
+    root.style.setProperty('--text-color', '#1e1e1e');
+    root.style.setProperty('--border-color', '#d72638');
+    document.body.classList.add('tema-claro');
+  }
+  
+  function ativarTemaEscuro() {
+    const root = document.documentElement;
+    root.style.setProperty('--bg-color', 'rgb(40, 40, 40)');
+    root.style.setProperty('--text-color', 'white');
+    root.style.setProperty('--border-color', 'gray');
+    document.body.classList.remove('tema-claro');
+  }
+  
+  document.getElementById('toggle-tema').addEventListener('click', () => {
+    const atual = getComputedStyle(document.documentElement)
+      .getPropertyValue('--bg-color')
+      .trim();
+  
+    if (atual === '#f8f9fa') {
+      ativarTemaEscuro();
+    } else {
+      ativarTemaClaro();
+    }
+  });
+  
+  
+
 function showmenu(){
     menu.classList.toggle('show');
 }
